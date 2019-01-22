d237dcff7903105c154262ff28b277ae35d6b4f5
Kurtis Angell <KurtisBlow@MacBook-Pro.local>
2019-01-22 12:05:52 +0000

    testing live build

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -102,7 +102,7 @@ class NavBar extends Component {
         {/* LOGIN MODAL BEGINS  */}
         {!this.state.login ? (
           <>
-            <Button onClick={this.toggleModal}>{this.props.buttonLabel} Login</Button>
+            <Button onClick={this.toggleModal}>{this.props.buttonLabel} X</Button>
             <Modal
               isOpen={this.state.modal}
               toggle={this.toggleModal}
